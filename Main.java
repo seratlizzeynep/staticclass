1387a964e66ff58473caa6f56c34228cc20be078
seratlizzeynep <zeynepaybuke.seratli@tedu.edu.tr>
2024-09-20 21:10:02 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+package staticclass;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Ogrenci.numara= "98765432"; // ogrenci classinda bunu static tanimladigim icin , mainde ogrenci nesnesi olusturmadan tanımlayabildik.
+	
+	
+		System.out.println("Öğrenci numarasi:" + Ogrenci.numara);
+	
+	}
+	
+
+}
---
+++ Ogrenci.java
@@ -0,0 +1,39 @@
+package staticclass;
+
+public class Ogrenci {
+
+	private int id;
+	
+	private String ad;
+	
+	private String soyad;
+	
+	public static String numara;
+	
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getAd() {
+		return ad;
+	}
+
+	public void setAd(String ad) {
+		this.ad = ad;
+	}
+
+	public String getSoyad() {
+		return soyad;
+	}
+
+	public void setSoyad(String soyad) {
+		this.soyad = soyad;
+	}
+	// alt shift s ile getter setterlari getirebiliriz.
+	
+}
